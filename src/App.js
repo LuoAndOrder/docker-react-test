dcb30279cdb15afba0ab418d36e60f64f5c88981
Kevin Luo <kwluo90@gmail.com>
2019-07-05 14:07:54 -0700

    First attempt at CI/CD with Travis and Fargate

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          Hello CI/CD!
         </p>
         <a
           className="App-link"
